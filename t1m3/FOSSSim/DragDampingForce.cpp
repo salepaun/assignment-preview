079832d522352c56d3d8cb67c1c990f053e22203
rga <rgarbacz@gmail.com>
2020-10-24 13:40:10 -0500

    Fix linear drag force for linearized implicit Euler

--- t1m3/FOSSSim/DragDampingForce.cpp
+++ t1m3/FOSSSim/DragDampingForce.cpp
@@ -1,5 +1,12 @@
 #include "DragDampingForce.h"
 
+#include <iostream>
+
+
+using namespace std;
+
+
+
 void DragDampingForce::addHessXToTotal( const VectorXs& x, const VectorXs& v, const VectorXs& m, MatrixXs& hessE )
 {
   assert( x.size() == v.size() );
@@ -20,4 +27,10 @@ void DragDampingForce::addHessVToTotal( const VectorXs& x, const VectorXs& v, co
   assert( x.size()%2 == 0 );
   
   // Compute the force Jacobian here!
+  int Size = x.size();
+  for(int i=0; i < Size; i++) {
+    hessE(i,i) -= m_b;
+  };
+
+  cout << "Called:" << __FUNCTION__ << ": H:" << hessE << endl;
 }
--- t1m3/FOSSSim/LinearizedImplicitEuler.cpp
+++ t1m3/FOSSSim/LinearizedImplicitEuler.cpp
@@ -20,12 +20,12 @@ inline static void dVAll( \
     VectorXs &_X, \
     VectorXs &_V, \
     MatrixXs const &_M, \
-    VectorXs const &_F, \
+    VectorXs const &_GradU, \
     MatrixXs const &_Hx, \
     MatrixXs const &_Hv, \
     scalar const &_dT)
 {
-  VectorXs V1 = _M.fullPivLu().solve(-_dT*_F);
+  VectorXs V1 = _M.fullPivLu().solve(-_dT*_GradU);
   _V += V1;
 }
 
@@ -36,7 +36,7 @@ inline static void dXAll( \
     VectorXs &_X, \
     VectorXs &_V, \
     MatrixXs const &_M, \
-    VectorXs const &_F, \
+    VectorXs const &_GradU, \
     MatrixXs const &_Hx, \
     MatrixXs const &_Hv, \
     scalar const &_dT)
@@ -53,14 +53,14 @@ inline static void dVStep( \
     VectorXs &_X, \
     VectorXs &_V, \
     MatrixXs const &_M, \
-    VectorXs const &_F, \
+    VectorXs const &_GradU, \
     MatrixXs const &_Hx, \
     MatrixXs const &_Hv, \
     scalar const &_dT)
 {
   for(int i=0, n=0; n < _NP; n++) {
     i = n << 1;
-    Vector2s V1n = _M.block<2,2>(i,i).fullPivLu().solve(-_dT*_F.segment<2>(i));
+    Vector2s V1n = _M.block<2,2>(i,i).fullPivLu().solve(-_dT*_GradU.segment<2>(i));
     _V.segment<2>(i) += V1n;
   };
 }
@@ -72,7 +72,7 @@ inline static void dXStep( \
     VectorXs &_X, \
     VectorXs &_V, \
     MatrixXs const &_M, \
-    VectorXs const &_F, \
+    VectorXs const &_GradU, \
     MatrixXs const &_Hx, \
     MatrixXs const &_Hv, \
     scalar const &_dT)
@@ -91,6 +91,7 @@ inline static void dXStep( \
  * Global variables.
  */
 bool g_bUseStep = false;
+scalar g_sTime = 0.0;
 
 
 
@@ -113,53 +114,63 @@ bool LinearizedImplicitEuler::stepScene( TwoDScene& scene, scalar dt )
   VectorXs dV = VectorXs::Zero(NDof);
   MatrixXs Hx = MatrixXs::Zero(NDof, NDof);
   MatrixXs Hv = MatrixXs::Zero(NDof, NDof);
-  VectorXs F = VectorXs::Zero(NDof);
+  VectorXs GradU = VectorXs::Zero(NDof);
+
+  scene.accumulateGradU(GradU, dX, dV);
+  scene.accumulateddUdxdx(Hx, dX, dV);
+  scene.accumulateddUdxdv(Hv, dX, dV);
 
 #ifndef NDEBUG
 #ifdef MY_DEBUG
-  cout << "Calling Udxdx" << endl;
-  scene.accumulateddUdxdx(Hx, dX, dV);
-  cout << "Calling Udxdv" << endl;
-  scene.accumulateddUdxdv(Hv, dX, dV);
-  cout << "Calling GradU" << endl;
-  scene.accumulateGradU(F, dX, dV);
-
   cout << "dX:" << dX << endl;
   cout << "dV:" << dV << endl;
-  cout << "F:" << F << endl;
+  cout << "GradU:" << GradU << endl;
   cout << "Hx:" << Hx << endl;
   cout << "Hv:" << Hv << endl;
 #endif
 #endif
 
+  FILE *Fd = energyDumpInit(g_sTime);
+
   // 
   // Mass matrix
   MatrixXs M1 = MatrixXs::Zero(NDof, NDof);
-  for(int i=0, j=0; i < NDof; ++i, ++j)
-    M1(i,j) = M(i);
+  for(int i=0; i < NDof; ++i)
+    M1(i,i) = M(i);
 
   M1 -= dt*(dt*Hx + Hv);
   // tests
   // MatrixXs MI = M1.inverse();
-  // VectorXs V1 = -dt * MI * F;
+  // VectorXs V1 = dt * MI * GradU;
 
   if(g_bUseStep && NP > 100) {
     // Works only when particles don't have common Hessian (each has it's own forces),
     // which was the case for all previous assignments
  
     // Calculating Vn+1
-    dVStep(NDof, NP, X, V, M1, F, Hx, Hv, dt);
+    dVStep(NDof, NP, X, V, M1, GradU, Hx, Hv, dt);
     // Calculating Xn+1
-    dXStep(NDof, NP, X, V, M1, F, Hx, Hv, dt);
+    dXStep(NDof, NP, X, V, M1, GradU, Hx, Hv, dt);
   } else {
     // Might require more CPU for more particles (??)
     // but can calculate relations between particles
  
     // Calculating Vn+1
-    dVAll(NDof, NP, X, V, M1, F, Hx, Hv, dt);
+    dVAll(NDof, NP, X, V, M1, GradU, Hx, Hv, dt);
     // Calculating Xn+1
-    dXAll(NDof, NP, X, V, M1, F, Hx, Hv, dt);
+    dXAll(NDof, NP, X, V, M1, GradU, Hx, Hv, dt);
   };
+
+
+  //
+  // Scene kinetic energy
+  scalar E = scene.computeKineticEnergy();
+  Vector2s VTotal = Vector2s::Zero();
+  for(int i=0; i < NDof; i+=2) {
+    VTotal += V.segment<2>(i);
+  };
+  energyDumpEnd(g_sTime, dt, Fd, E, VTotal);
+
 
 #ifndef NDEBUG
 #ifdef MY_DEBUG
