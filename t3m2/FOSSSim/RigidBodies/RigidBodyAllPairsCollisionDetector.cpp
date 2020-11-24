872b0c907f7cd8915df6ff8632bfc6b6820b9739
rga <rgarbacz@gmail.com>
2020-11-24 00:14:15 -0600

    Add week t3m2 assignment

---
+++ t3m2/FOSSSim/RigidBodies/RigidBodyAllPairsCollisionDetector.cpp
@@ -0,0 +1,7 @@
+#include "RigidBodyAllPairsCollisionDetector.h"
+
+void RigidBodyAllPairsCollisionDetector::detectCollisions( const std::vector<RigidBody>& rbs, std::set<RigidBodyCollision>& collisions )
+{
+  // Your code goes here! 
+  // Compute all vertex-edge collisions between all pairs of rigid bodies.
+}
