65ba1c561ed97da02781f664be1e52fd89f480da
rga <rgarbacz@gmail.com>
2020-12-06 10:53:14 -0600

    Progress with t5m1

--- t5m1/FOSSSim/StableFluids/StableFluidsSim.cpp
+++ t5m1/FOSSSim/StableFluids/StableFluidsSim.cpp
@@ -1,6 +1,12 @@
  #include "StableFluidsSim.h"
 #include <Eigen/LU>
 
+
+#define LERP(a,b,x) (1-x)*a + x*b;
+
+
+
+
 void StableFluidsSim::diffuseD(int N, ArrayXs * x, ArrayXs * x0, scalar diff, scalar dt)
 {
   assert((*x0 == *x0).all());
@@ -16,7 +22,7 @@ void StableFluidsSim::diffuseD(int N, ArrayXs * x, ArrayXs * x0, scalar diff, sc
       {
         // Your code goes here!
         // Do diffuse for ([1, N], [1, N])
-        x(i,j) = x0(i,j) + a * (x(i-1,j)+x(i+1,j)+x(i,j-1)+x(i,j+1)) / (1+4*a);
+        (*x)(i,j) = (*x0)(i,j) + a * ((*x)(i-1,j)+(*x)(i+1,j)+(*x)(i,j-1)+(*x)(i,j+1)) / (1+4*a);
       }
     }
   }
@@ -38,11 +44,11 @@ void StableFluidsSim::diffuseU(int N, ArrayXs * x, ArrayXs * x0, scalar diff, sc
         // Your code goes here! 
         // Do diffuse for ([1, N], [0, N]), note the case when (j == 0) or (j == N) need special treatment
         if (j == 0)
-          ;
+          (*x)(i,j) = (*x0)(i,j) + a * ((*x)(i-1,j)+(*x)(i+1,j)+(*x)(i,j+1)) / (1+3*a);
         else if (j == N)
-          ;
+          (*x)(i,j) = (*x0)(i,j) + a * ((*x)(i-1,j)+(*x)(i+1,j)+(*x)(i,j-1)) / (1+3*a);
         else
-          ;
+          (*x)(i,j) = (*x0)(i,j) + a * ((*x)(i-1,j)+(*x)(i+1,j)+(*x)(i,j-1)+(*x)(i,j+1)) / (1+4*a);
       }
     }
   }
@@ -63,11 +69,11 @@ void StableFluidsSim::diffuseV(int N, ArrayXs * x, ArrayXs * x0, scalar diff, sc
       {
         // Your code goes here!
         if (i == 0)
-          ; 
+          (*x)(i,j) = (*x0)(i,j) + a * ((*x)(i+1,j)+(*x)(i,j-1)+(*x)(i,j+1)) / (1+3*a);
         else if (i == N)
-          ; 
+          (*x)(i,j) = (*x0)(i,j) + a * ((*x)(i-1,j)+(*x)(i,j-1)+(*x)(i,j+1)) / (1+3*a);
         else
-          ;
+          (*x)(i,j) = (*x0)(i,j) + a * ((*x)(i-1,j)+(*x)(i+1,j)+(*x)(i,j-1)+(*x)(i,j+1)) / (1+4*a);
       }
     }
   }
@@ -81,7 +87,7 @@ void StableFluidsSim::advectD(int N, ArrayXs * x, ArrayXs * x0, ArrayXs * u, Arr
   
   // Your code goes here!
   // Advect for ([1, N], [1, N])
-  float dt0, px, py;
+  double dt0, px, py;
 
   dt0 = dt*N;
 
@@ -89,9 +95,9 @@ void StableFluidsSim::advectD(int N, ArrayXs * x, ArrayXs * x0, ArrayXs * u, Arr
   {
     for (int j = 1; j <= N; j++)
     {
-      px = i - dt0 * u(i,j);
-      py = j - dt0 * v(i,j);
-      x(i,j) = interpolateD(x0, px, py);
+      px = i - dt0 * (*u)(i,j);
+      py = j - dt0 * (*v)(i,j);
+      (*x)(i,j) = interpolateD(x0, px, py);
     }
   }
 }
@@ -101,26 +107,67 @@ scalar StableFluidsSim::interpolateD(ArrayXs * d, scalar i, scalar j)
   // Your code goes here!
   // Note the indices should be CLAMP-ed to [0, m_N], since we have to use (i + 1) and (j + 1)
   int i0, j0, i1, j1;
-  float s0, s1, t0, t1;
 
+  /*
+  double s0, s1, t0, t1;
   i0 = CLAMP(int(i), 0, m_N);
   i1 = int(i) + 1;
   j0 = CLAMP(int(j), 0, m_N);
   j1 = int(j) + 1;
 
   s1 = i-i0; s0 = 1-s1; t1 = j-j0; t0=1-t1;
-  return s0 * (t0*d(i0,j0) + t1*d(i0,j1)) + s1 * (t0*d(i1,j0) + t1*d(i1,j1));
+  return s0 * (t0*(*d)(i0,j0) + t1*(*d)(i0,j1)) + s1 * (t0*(*d)(i1,j0) + t1*(*d)(i1,j1));
+  */
+
+  double s, t, d0, d1;
+
+  i0 = CLAMP(int(i), 0, m_N);
+  i1 = i0 + 1;
+  j0 = CLAMP(int(j), 0, m_N);
+  j1 = j0 + 1;
+  s = CLAMP(i-i0, 0, 1);
+  t = CLAMP(j-j0, 0, 1);
+  d0 = LERP((*d)(i0,j0), (*d)(i1,j0), s);
+  d1 = LERP((*d)(i0,j1), (*d)(i1,j1), s);
+      
+  return LERP(d0, d1, t);
 }
 
 scalar StableFluidsSim::interpolateU(ArrayXs * u, scalar i, scalar j)
 {
-    // Your code goes here! 
-    // Note the i index should be CLAMP-ed to [0, m_N], while j index should be CLAMP-ed to [0, m_N-1], since we have to use (i + 1) and (j + 1)
+  // Your code goes here! 
+  // Note the i index should be CLAMP-ed to [0, m_N], while j index should be CLAMP-ed to [0, m_N-1], since we have to use (i + 1) and (j + 1)
+  int i0, j0, i1, j1;
+  double s, t, u0, u1;
+
+  i0 = CLAMP(int(i), 0, m_N);
+  i1 = i0 + 1;
+  j0 = CLAMP(int(j-0.5), 0, m_N-1);
+  j1 = j0 + 1;
+  s = CLAMP(i-i0, 0, 1);
+  t = CLAMP(j-j0-0.5, 0, 1);
+  u0 = LERP((*u)(i0,j0), (*u)(i1,j0), s);
+  u1 = LERP((*u)(i0,j1), (*u)(i1,j1), s);
+
+  return LERP(u0, u1, t);
 }
 
 scalar StableFluidsSim::interpolateV(ArrayXs * v, scalar i, scalar j)
 {
-    // Your code goes here!
+  // Your code goes here!
+  int i0, j0, i1, j1;
+  double s, t, v0, v1;
+
+  i0 = CLAMP(int(i-0.5), 0, m_N-1);
+  i1 = i0 + 1;
+  j0 = CLAMP(int(j), 0, m_N);
+  j1 = j0 + 1;
+  s = CLAMP(i-i0-0.5, 0, 1);
+  t = CLAMP(j-j0, 0, 1);
+  v0 = LERP((*v)(i0,j0), (*v)(i1,j0), s);
+  v1 = LERP((*v)(i0,j1), (*v)(i1,j1), s);
+
+  return LERP(v0, v1, t);
 }
 
 void StableFluidsSim::advectU(int N, ArrayXs * x, ArrayXs * x0, ArrayXs * u, ArrayXs * v, scalar dt)
@@ -129,6 +176,9 @@ void StableFluidsSim::advectU(int N, ArrayXs * x, ArrayXs * x0, ArrayXs * u, Arr
   assert((*u == *u).all());
   assert((*v == *v).all());
   
+  double ux, uy, dt0;
+  dt0 = dt*N;
+
   for (int i = 1; i <= N; i++)
   {
     for (int j = 0; j <= N; j++)
@@ -136,6 +186,9 @@ void StableFluidsSim::advectU(int N, ArrayXs * x, ArrayXs * x0, ArrayXs * u, Arr
       // Your code goes here!
       
       // Add the origin of U grid to the coordinate before sampling, for example, sample at (i + 0, j + 0.5) when you need backtracing the old velocity at (i, j)
+      ux = i - dt0 * (*u)(i,j);
+      uy = j - dt0 * (*v)(i,j);
+      (*x)(i,j) = interpolateU(x0, ux, uy);
       
       // Now you have the backward-traced velocity, minus it from the current position (i + 0, j + 0.5), then sample the velocity again.
     }
